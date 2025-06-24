930cf1b9b6516bcfb43b70c343fb9cfcabff9b50
Naveen Kumar <knaveenk869@gmail.com>
2025-06-24 14:33:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // your code here
-document.getElementById("myForm").addEventListener('submit',function(event){
+document.getElementById('myForm').addEventListener('submit',function(event){
 	event.preventDefault();
-	const name = encodeURIComponent(document.getElementById("name").value);
-	const year = encodeURIComponent(document.getElementById("year").value);
+	const name = encodeURIComponent(document.getElementById('name').value);
+	const year = encodeURIComponent(document.getElementById('year').value);
 	const baseUrl = 'https://localhost:8080/';
 	const query = `?name=${name}&year=${year}`;
 	document.getElementById('url').textContent = baseUrl+query;
 });
-}
