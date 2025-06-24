2a747b887a69ca463559a01be1c38711e25155f7
Naveen Kumar <knaveenk869@gmail.com>
2025-06-24 14:31:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("myForm").addEventListener('submit',function(event){
 	event.preventDefault();
 	const name = encodeURIComponent(document.getElementById("name").value);
 	const year = encodeURIComponent(document.getElementById("year").value);
-	const baseUrl = 'https://localhost:8080/'
+	const baseUrl = 'https://localhost:8080/';
 	const query = `?name=${name}&year=${year}`;
 	document.getElementById('url').textContent = baseUrl+query;
 });
