fe5ea66b24b190162d8bf076da983cbe70a46b55
Naveen Kumar <knaveenk869@gmail.com>
2025-06-24 14:30:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // your code here
 document.getElementById("myForm").addEventListener('submit',function(event){
 	event.preventDefault();
-	const name = document.getElementById("name").value;
-	const year = document.getElementById("year").value;
+	const name = encodeURIComponent(document.getElementById("name").value);
+	const year = encodeURIComponent(document.getElementById("year").value);
 	const baseUrl = 'https://localhost:8080/'
 	const query = `?name=${name}&year=${year}`;
 	document.getElementById('url').textContent = baseUrl+query;
